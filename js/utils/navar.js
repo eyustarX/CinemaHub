327fef965144c8820b884708bdde5c50193aa6c3
Eyob Tamiru <eyobtamiru111@gmail.com>
2026-08-09 17:55:36 +0300

    Implement authentication link rendering and logout functionality

---
+++ js/utils/navar.js
@@ -0,0 +1,25 @@
+import { getCurrentUser, logout } from "../data/auth.js";
+
+const authLink = document.getElementById("navbar-auth-link");
+
+function renderAuthLink() {
+  const user = getCurrentUser();
+
+  if (user) {
+    authLink.textContent = `Log Out (${user.name})`;
+    authLink.href = "#";
+    authLink.onclick = handleLogoutClick;
+  } else {
+    authLink.textContent = "Log In";
+    authLink.href = "login.html";
+    authLink.onclick = null;
+  }
+}
+
+function handleLogoutClick(event) {
+  event.preventDefault();
+  logout();
+  window.location.href = "index.html";
+}
+
+renderAuthLink();
